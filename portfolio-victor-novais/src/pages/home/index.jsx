4eb1e6cc00d0fa3b19bee2a3c5c0e944121bbbb7
VictorNovais <victornovais1337@gmail.com>
2024-04-23 23:27:05 -0300

    up

--- portfolio-victor-novais/src/pages/home/index.jsx
+++ portfolio-victor-novais/src/pages/home/index.jsx
@@ -10,13 +10,28 @@ export default function Home() {
                 <div className="home-title hero_cont animate__animated animate__fadeIn animate__slideInDown animate__delay-1s">
 
                     <div className="line h1-60">
-                        <span >F</span><span>r</span><span>o</span><span>n</span><span>t</span><span>-</span><span>E</span><span>n</span><span>d</span>
+                        <span >F</span>
+                        <span>r</span>
+                        <span>o</span>
+                        <span>n</span>
+                        <span>t</span>
+                        <span>-</span>
+                        <span>E</span>
+                        <span>n</span>
+                        <span>d</span>
                     </div>
                     <div className="line h1-60">
                         <span>&</span>
                     </div>
                     <div className="line h1-60">
-                        <span>B</span><span>a</span><span>c</span><span>k</span><span>-</span><span>E</span><span>n</span><span>d</span>
+                        <span>B</span>
+                        <span>a</span>
+                        <span>c</span>
+                        <span>k</span>
+                        <span>-</span>
+                        <span>E</span>
+                        <span>n</span>
+                        <span>d</span>
                     </div>
 
                     <div className="line h1-60">
