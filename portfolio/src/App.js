63bd1e3bec86bccd79c7a2032c19284e494811a7
VictorNovais <victornovais1337@gmail.com>
2024-07-31 18:09:00 -0300

    contact

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ContactResume from './pages/contact-resume/contact';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactResume />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/pages/contact-resume/contact.jsx
@@ -0,0 +1,32 @@
+import './contact.css';
+
+import perfil from "../../assets/perfil.jpeg";
+import instagram from "../../assets/instagram.png";
+import twitter from "../../assets/twitter.png";
+import gitHub from "../../assets/github.png";
+import linkedin from "../../assets/linkedin-logo.png";
+import email from "../../assets/email.png";
+
+export default function ContactResume() {
+    return (
+        <div className="contact-bar">
+            <header className='contact-header'>
+                <h1>NOVAIS</h1>
+                <span>Fullstack Developer</span>
+            </header>
+            <img className='perfil' src={perfil} alt="Perfil" />
+            <h2>victornovais1337@gmail.com</h2>
+            <h2>Based in Salvador, Bahia, Brazil</h2>
+            <ul className='social'>
+                <li><a href="https://www.instagram.com/novais_.dev/" target="_blank" rel="noopener noreferrer"><img src={instagram} className="icon" alt="Instagram" /></a></li>
+                <li><a href="https://x.com/VictorNova38306" target="_blank" rel="noopener noreferrer"><img src={twitter} className="icon" alt="Twitter" /></a></li>
+                <li><a href="https://github.com/Victor-Novais" target="_blank" rel="noopener noreferrer"><img src={gitHub} className="icon" alt="GitHub" /></a></li>
+                <li><a href="https://www.linkedin.com/in/victor-novais-/" target="_blank" rel="noopener noreferrer"><img src={linkedin} className="icon" alt="LinkedIn" /></a></li>
+            </ul>
+
+            <a href="mailto:victornovais1337@gmail.com?subject=Contrate-me&body=Olá,%20gostaria%20de%20discutir%20uma%20oportunidade." className="theme-btn">
+                <img className="icon-email" src={email} alt="Email" /><span>Contrate-me!</span>
+            </a>
+        </div>
+    );
+}
