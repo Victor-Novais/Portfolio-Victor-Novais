05488b240d79927b29c9c4e6f29cda72f75bd6ae
VictorNovais <victornovais1337@gmail.com>
2024-09-07 14:19:54 -0300

    site para celular

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,73 +1,74 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import Menu from './components/menu/menu.jsx';
-import About from './pages/about/about.jsx';
-import ContactResume from './pages/contact-resume/contact';
-import Contact from './pages/contact/contact.jsx';
-import Portfolio from './pages/portfolio/portfolio.jsx';
-import Presentation from './pages/presentation/presentation.jsx';
-import Skills from './pages/skills/skills.jsx';
+import React, { useEffect } from "react";
+import "./App.css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Menu from "./components/menu/menu.jsx";
+import About from "./pages/about/about.jsx";
+import ContactResume from "./pages/contact-resume/contact";
+import Contact from "./pages/contact/contact.jsx";
+import Portfolio from "./pages/portfolio/portfolio.jsx";
+import Presentation from "./pages/presentation/presentation.jsx";
+import Skills from "./pages/skills/skills.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   useEffect(() => {
-    // Animating sections
-    gsap.utils.toArray('.section').forEach(section => {
-      gsap.fromTo(section,
+    gsap.utils.toArray(".section").forEach((section) => {
+      gsap.fromTo(
+        section,
         { opacity: 0, y: 50 },
         {
           opacity: 1,
           y: 0,
           duration: 1.5,
-          ease: 'power4.out',
+          ease: "power4.out",
           scrollTrigger: {
             trigger: section,
-            start: 'top 90%',
-            toggleActions: 'play none none none',
+            start: "top 90%",
+            toggleActions: "play none none none",
             onEnter: () => setActiveMenu(section.id),
-            onEnterBack: () => setActiveMenu(section.id)
-          }
+            onEnterBack: () => setActiveMenu(section.id),
+          },
         }
       );
     });
 
-    // Function to set active menu item
     function setActiveMenu(sectionId) {
-      document.querySelectorAll('.icon-menu').forEach(icon => {
-        icon.classList.remove('active');
+      document.querySelectorAll(".icon-menu").forEach((icon) => {
+        icon.classList.remove("active");
       });
-      const activeIcon = document.querySelector(`#menu-${sectionId} .icon-menu`);
+      const activeIcon = document.querySelector(
+        `#menu-${sectionId} .icon-menu`
+      );
       if (activeIcon) {
-        activeIcon.classList.add('active');
+        activeIcon.classList.add("active");
       }
     }
 
-    // Animating ContactResume
-    const contactResume = document.querySelector('.contact-resume');
+    const contactResume = document.querySelector(".contact-resume");
     if (contactResume) {
-      gsap.fromTo(contactResume,
+      gsap.fromTo(
+        contactResume,
         { opacity: 0, scale: 0.8 },
         {
           opacity: 1,
           scale: 1,
           duration: 1.5,
-          ease: 'power4.out',
+          ease: "power4.out",
           scrollTrigger: {
             trigger: contactResume,
-            start: 'top 80%',
-            toggleActions: 'play none none none'
-          }
+            start: "top 80%",
+            toggleActions: "play none none none",
+          },
         }
       );
     }
-
   }, []);
 
   return (
-    <div className="App"><ContactResume />
+    <div className="App">
+      <ContactResume />
       <Menu />
       <div id="home" className="section">
         <Presentation />
@@ -77,13 +78,12 @@ function App() {
       </div>
       <div id="skills" className="section">
         <Skills />
-      </div>
+      </div>{" "}
       <div id="portfolio" className="section">
         <Portfolio />
       </div>
       <div id="contact" className="section">
         <Contact />
-
       </div>
     </div>
   );
--- portfolio/src/pages/portfolio/portfolio.jsx
+++ portfolio/src/pages/portfolio/portfolio.jsx
@@ -1,79 +1,127 @@
-import './portfolio.css';
-import list from "../../assets/list.png"
-import project1 from "../../assets/project 1.png"
-import project2 from "../../assets/project-2.png"
-import project3 from "../../assets/project-3.png"
-import seta from "../../assets/seta-direita.png"
+import "./portfolio.css";
+import list from "../../assets/list.png";
+import project1 from "../../assets/project 1.png";
+import project2 from "../../assets/project-2.png";
+import project3 from "../../assets/project-3.png";
+import seta from "../../assets/seta-direita.png";
 export default function Portfolio() {
-    return (
-        <div className="hero-portfolio">
-            <h4 className="hero-h4-portfolio">
-                <img src={list} alt="" /><i>portfolio</i>
-            </h4>
-            <div>
-                <header className="header-skills">Meus <span className="span-text-about">Projetos</span></header>
-                <div className='portfolio-how'>
-                    <div className='portfolio-item-1'>
-                        <div className="image-box">
-                            <img src={project1} alt="" />
-                        </div>
-                        <div className="content-box"> <a href="https://github.com/Victor-Novais/controle-de-caixa" target="_blank" rel="noopener noreferrer" >
-                            <div>
-                                <h3 className="portfolio-title">Loja Urban</h3>
-                                <p>O projeto se trata de um sistema de controle de fluxo de vendas, despesas e estoque.</p>
-                            </div>
-                            <img src={seta} className='seta-ir' alt="" /></a>
-                        </div>
-                        <ul className='skill-project'>
-                            <li>JavaScript</li>
-                            <li>React</li>
-                            <li>Node</li>
-                            <li>Sql</li>
-                        </ul>
-                    </div>
+  document.addEventListener("DOMContentLoaded", function () {
+    // Seleciona todos os itens de portfólio
+    const portfolioItems = document.querySelectorAll(".portfolio-item");
 
-                    <div className='portfolio-item-2'>
-                        <div className="image-box">
-                            <img src={project2} alt="" />
-                        </div>
-                        <div className="content-box">
-                            <a href="https://github.com/Victor-Novais/PayWise-FrontEnd" target="_blank" rel="noopener noreferrer" > <div>
-                                <div>
-                                    <h3 className="portfolio-title">Pay Wise</h3>
-                                    <p>Criado parauma empresa a gerenciar finanças
-                                        de todos os usuários.</p></div>
-                            </div>
-                                <img src={seta} className='seta-ir' alt="" /></a>
-                        </div>
-                        <ul className='skill-project'>
-                            <li>JavaScript</li>
-                            <li>React</li>
-                            <li>Node</li>
-                            <li>Sql</li>
-                        </ul>
-                    </div>
+    portfolioItems.forEach((item) => {
+      item.addEventListener("click", function () {
+        // Remove a classe active de todos os itens antes de adicionar ao item clicado
+        portfolioItems.forEach((i) => i.classList.remove("active"));
 
-                    <div className='portfolio-item-3'>
-                        <div className="image-box">
-                            <img src={project3} alt="" />
-                        </div>
-                        <div className="content-box">
-                            <a href="https://github.com/Victor-Novais/dindin-bank" target="_blank" rel="noopener noreferrer">
-                                <div>
-                                    <h3 className="portfolio-title">SecureBank</h3>
-                                    <p>Este projeto permite que usuários cadastrados gerenciem suas finanças pessoais.</p>
-                                </div>
-                                <img src={seta} className='seta-ir' alt="" /></a>
-                        </div>
-                        <ul className='skill-project'>
-                            <li>JavaScript</li>
-                            <li>React</li>
-                            <li>Node</li>
-                            <li>Sql</li>
-                        </ul>
-                    </div>
-                </div>
+        // Adiciona ou remove a classe active ao item clicado
+        if (!this.classList.contains("active")) {
+          this.classList.add("active");
+        }
+      });
+    });
+  });
+
+  return (
+    <div className="hero-portfolio">
+      <h4 className="hero-h4-portfolio">
+        <img src={list} alt="" />
+        <i>portfolio</i>
+      </h4>
+      <div>
+        <header className="header-skills">
+          Meus <span className="span-text-about">Projetos</span>
+        </header>
+        <div className="portfolio-how">
+          <div className="portfolio-item-1">
+            <div className="image-box">
+              <img src={project1} alt="" />
             </div>
-        </div >
-    );
+            <div className="content-box">
+              {" "}
+              <a
+                href="https://github.com/Victor-Novais/controle-de-caixa"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>
+                  <h3 className="portfolio-title">Loja Urban</h3>
+                  <p>
+                    O projeto se trata de um sistema de controle de fluxo de
+                    vendas, despesas e estoque.
+                  </p>
+                </div>
+                <img src={seta} className="seta-ir" alt="" />
+              </a>
+            </div>
+            <ul className="skill-project">
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Node</li>
+              <li>Sql</li>
+            </ul>
+          </div>
+
+          <div className="portfolio-item-2">
+            <div className="image-box">
+              <img src={project2} alt="" />
+            </div>
+            <div className="content-box">
+              <a
+                href="https://github.com/Victor-Novais/PayWise-FrontEnd"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <div>
+                  <div>
+                    <h3 className="portfolio-title">Pay Wise</h3>
+                    <p>
+                      Criado parauma empresa a gerenciar finanças de todos os
+                      usuários.
+                    </p>
+                  </div>
+                </div>
+                <img src={seta} className="seta-ir" alt="" />
+              </a>
+            </div>
+            <ul className="skill-project">
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Node</li>
+              <li>Sql</li>
+            </ul>
+          </div>
+
+          <div className="portfolio-item-3">
+            <div className="image-box">
+              <img src={project3} alt="" />
+            </div>
+            <div className="content-box">
+              <a
+                href="https://github.com/Victor-Novais/dindin-bank"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div>
+                  <h3 className="portfolio-title">SecureBank</h3>
+                  <p>
+                    Este projeto permite que usuários cadastrados gerenciem suas
+                    finanças pessoais.
+                  </p>
+                </div>
+                <img src={seta} className="seta-ir" alt="" />
+              </a>
+            </div>
+            <ul className="skill-project">
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Node</li>
+              <li>Sql</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
